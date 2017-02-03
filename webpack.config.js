d5d3ff72ed4d4d1f33f15c2b216febea921077c3
cmcco066 <cmcco066@uottawa.ca>
2017-02-03 07:25:37 -0500

    Added basic webpack config
    Added the basic webpack config. Changes will be required though.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app-client.js'),
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: path.join(__dirname, 'src'),
+      loader: ['babel-loader'],
+      query: {
+        cacheDirectory: 'babel_cache',
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  plugins: [
+
+  ]
+};
