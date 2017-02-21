d19f475ca2ab0f44026abec9a5323f91a31017d7
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-02-21 16:02:23 -0500

    Fixed line endings and gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,35 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scripts', function() {
-    return gulp.src('public/assets/js/scripts.js')
+    return gulp.src('public/assets/js/concat/*.js')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
         })))
-        .pipe(uglify())
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('public/assets/js'));
 });
 
 gulp.task('styles', function() {
-    return gulp.src('public/assets/scss/app.scss')
+    return gulp.src('public/assets/scss/styles.scss')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
         })))
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('public/assets/css'));
 });
 
 gulp.task('watch', ['scripts', 'styles'], function() {
-    gulp.watch('public/assets/js/*.js', ['scripts']);
+    gulp.watch('public/assets/js/concat/*.js', ['scripts']);
     gulp.watch('public/assets/scss/*.scss', ['styles']);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,12 @@ module.exports = {
     },
     module: {
         loaders: [{
-                test: /\.json$/,
-                loader: 'json-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            }
-        ]
+            test: /\.json$/,
+            loader: 'json-loader'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
     }
 };
