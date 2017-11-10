c65c65a85a28a7233aab00c472d844417fe6f2de
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-11-09 22:11:46 -0500

    Merge branch 'dev-cody' into dev-ryan

---
+++ client/src/__tests__/components/projects/Product.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Product from '../../../Components/projects/Product';
+
+describe('<Note />', () => {
+  let location;
+  
+  beforeAll(() => {
+    // Create location prop with needed data
+    location = {
+      match: {
+        params: {
+          id: 1
+        }
+      }
+    };
+  });
+
+  it('should render correctly', () => {   
+    // Create mock functions / spies
+    const setActiveSubtabMock = jest.fn();
+    const renderErrorMock = jest.fn();
+    
+    // Shallow render it
+    const wrapper = shallow(
+      <Product setActiveSubtab={setActiveSubtabMock} renderError={renderErrorMock} location={location}/>
+    );
+
+    // Compare to old snapshot
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+
+    // Check set active subtab is called and with correct value
+    expect(setActiveSubtabMock).toHaveBeenCalledTimes(1);
+    expect(setActiveSubtabMock.mock.calls[0][0]).toBe(0);
+  });
+});
+
